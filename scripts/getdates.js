38f7a025d7b3835c93936e713f8eb48b83304c9a
EngrOdus <henry.odumamwen@outlook.com>
2025-09-09 05:54:53 +0100

    Dynamically developed and displayed current date and last modified date using javaScript date object.

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const date = new Date();
+let currentYear = date.getFullYear();
+let text = currentYear.toString();
+document.getElementById("currentyear").innerHTML = text;
+
+let lastModify = new Date(document.lastModified);
+let lastModifyString = "Last Updated:" + " " + lastModify.toString();
+document.getElementById("lastModified").innerHTML = lastModifyString;
