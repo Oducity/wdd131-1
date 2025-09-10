4cc9240edac4f14ebafbad7aef03d3cd52a2681f
EngrOdus <henry.odumamwen@outlook.com>
2025-09-10 16:48:26 +0100

    Added json file.

---
+++ week02/practice.js
@@ -0,0 +1,13 @@
+const Day = 6;
+const limit = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+for (let i = 0; i < studentReport.length(); i++){
+    if (studentReport[i] < limit) {
+        console.log(studentReport[i]);
+    }
+}
+
+//studentReport.forEach(function (studentReport, index) {
+//    console.log(studentReport[index]);
+//})
