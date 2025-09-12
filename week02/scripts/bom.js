c5f0fa072779303a518c0e95750a00f25986231a
EngrOdus <henry.odumamwen@outlook.com>
2025-09-12 17:15:39 +0100

    created the button addEventListener().

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,22 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("ul");
-
-
 const li = document.createElementNS("li");
+
 const deleteButton = document.createElement("li");
 list.textContent = input.value;
 deleteButton.textContent = "✖";
 li.append(deleteButton);
+
+
+button.addEventListener("click", function () {
+    
+    if (input.value.trim() !== "") {
+        return input.focus();
+    }
+});
+
+deleteButton.addEventListener("click", function () {
+    list.removeChild(li);
+    input.focus();
+})
