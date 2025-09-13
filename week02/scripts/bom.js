1d971fcb3fe63547a51792f2e9c16c4c031310d2
EngrOdus <henry.odumamwen@outlook.com>
2025-09-13 06:23:53 +0100

    moved javascript code into the if conditional statement.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,19 +1,21 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("ul");
-const li = document.createElementNS("li");
+const li = document.createElement("li");
+const deleteButton = document.createElement("button");
 
-const deleteButton = document.createElement("li");
-list.textContent = input.value;
-deleteButton.textContent = "✖";
-li.append(deleteButton);
 
+
+//================================================================
 
 button.addEventListener("click", function () {
     
     if (input.value.trim() !== "") {
-        return input.focus();
+        li.textContent = input.value;
+        deleteButton.textContent = "✖";
+        li.append(deleteButton);
     }
+    return input.focus();
 });
 
 deleteButton.addEventListener("click", function () {
