62d8e3ae73edb9fb076eedaf5a8a69394036d46f
EngrOdus <henry.odumamwen@outlook.com>
2025-09-16 13:02:53 +0100

    Developed javascript to dynamically display menu in mobile view devices.

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,12 @@ document.getElementById("currentyear").innerHTML = text;
 let lastModify = new Date(document.lastModified);
 let lastModifyString = "Last Updated:" + " " + lastModify.toString();
 document.getElementById("lastModified").innerHTML = lastModifyString;
+
+
+const nav = document.querySelector(".nav");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+  nav.classList.toggle("nav--display");
+  hamburger.classList.toggle("hamburger-quit");
+});
