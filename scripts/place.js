c8cb49bc397aee1ca73eefc7fe6af5267b5bd3cf
EngrOdus <henry.odumamwen@outlook.com>
2025-09-24 17:21:32 +0100

    Developed function calculateWindChill() in the place.js file.

--- scripts/place.js
+++ scripts/place.js
@@ -24,3 +24,20 @@ document.getElementById("time-zone-value").innerText = timeZone;
 document.getElementById("currency-value").innerText = currency;
 document.getElementById("call-code-value").innerText = callCode;
 document.getElementById("internet-value").innerText = internet;
+
+let temperature = 0;
+let velocity = 0;
+let windChill;
+
+function calculateWindChill() {
+    let value = 35.74 + 0.6215 * temperature - 35.75(velocity ** 0.16) + 0.4275 * temperature(velocity ** 0.16);
+    return value;
+}
+
+if (temperature <= 50 && velocity > 3) {
+    windChill = calculateWindChill();
+} else {
+    windChill = NaN;
+}
+
+//Wind Chill (°F) = 35.74 + 0.6215T – 35.75(V^0.16) + 0.4275T(V^0.16)//
