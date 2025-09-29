5f6b7265c1a691a770a52ca41e3f4d64da84d89d
EngrOdus <henry.odumamwen@outlook.com>
2025-09-29 11:56:43 +0100

    Example use cases of array.filter(), array.map() methods.

--- week02/practice/practice1.js
+++ week02/practice/practice1.js
@@ -1,5 +1,17 @@
 
 /* Remender operation where the % symbol is the remender opretor.*/
+
+/* Below is an example of the use of array.filter() method */
+
+function isBigEnough(value) {
+    return value >= 10;
+}
+
+const nums = [12, 5, 8, 130, 44];
+console.log(nums.filter(isBigEnough));
+
+/* Another example of array.filter() method use case. */
+
 const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 function isPrime(n) {
@@ -19,3 +31,10 @@ function isPrime(n) {
 }
 
 console.log(araay.filter(isPrime));
+
+
+/* Array.map() method simple use */
+
+const array1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const mapped = array1.map((value) => parseInt(value, 10));
+console.log(mapped);
