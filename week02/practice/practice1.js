7b5c9d5c5196f70cfcf98a0cc437b687ad7d7b9a
EngrOdus <henry.odumamwen@outlook.com>
2025-09-29 07:38:42 +0100

    created practice1.js for filter() method practice purpose.

---
+++ week02/practice/practice1.js
@@ -0,0 +1,21 @@
+
+/* Remender operation where the % symbol is the remender opretor.*/
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+    if (n % 2 === 0) {
+        return n === 2;
+    }
+
+    for (let factor = 3; factor * factor <= n; factor += 2) {
+        if (n % factor === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(araay.filter(isPrime));
