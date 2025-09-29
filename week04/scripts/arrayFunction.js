dca583afc4923bad9fce9357fb8e799ee22e41a2
EngrOdus <henry.odumamwen@outlook.com>
2025-09-29 14:30:54 +0100

    Added comment to the arraFunction.js file for documentation.

--- week04/scripts/arrayFunction.js
+++ week04/scripts/arrayFunction.js
@@ -8,3 +8,13 @@ console.log(namesLength);
 
 const initial = 0;
 let averageStringLength = console.log(names.reduce((acumm, curVal) => acumm + curVal.length, initial) / names.length);
+
+
+/* Below is the explanation of the averageStringLength operation:
+The initial value of the reduction is set to 0. Inside the
+reducer function, total represents the accumulated sum of
+string lengths, and name represents each individual name in
+the array. The reducer function adds the length of each name
+to the total in each iteration.
+(like any accumulator, total += name.length).
+*/
