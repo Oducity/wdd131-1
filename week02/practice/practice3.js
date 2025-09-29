0dabbc2849041bb2615ec48aec2787de1e92fd6a
EngrOdus <henry.odumamwen@outlook.com>
2025-09-29 16:19:47 +0100

    Did some play arround in the practice.js file in the week02 directory.

---
+++ week02/practice/practice3.js
@@ -0,0 +1,7 @@
+let esomeprazole = { 'id': 'mnb78932', 'amount': 23145, 'amountType': 'ct', 'expDate': '10/01/2021' };
+
+let dose = esomeprazole['amount']
+
+
+let dose1 = esomeprazole.amount
+console.log(dose1);
