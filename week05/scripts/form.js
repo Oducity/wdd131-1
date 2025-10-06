d517174c381953bd95f538b69d2075a23260550c
EngrOdus <henry.odumamwen@outlook.com>
2025-10-06 17:51:04 +0100

    Developed and completed the form.css file.

--- week05/scripts/form.js
+++ week05/scripts/form.js
@@ -6,3 +6,48 @@ document.getElementById("currentyear").innerHTML = text;
 let lastModify = new Date(document.lastModified);
 let lastModifyString = "Last Updated:" + " " + lastModify.toString();
 document.getElementById("lastModified").innerHTML = lastModifyString;
+
+
+
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+createProductOptions(products);
+
+function createProductOptions(setOptions) {
+    setOptions.forEach(product => {
+        let option = document.createElement("option");
+
+        option.setAttribute("id", product.id);
+        option.setAttribute("value", product.name);
+        option.innerText = product.name;
+
+        document.querySelector("#product-name").appendChild(option);
+    });
+};
