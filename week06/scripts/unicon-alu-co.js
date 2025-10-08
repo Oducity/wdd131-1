7a7cf65d82e85d52ac2b9d3653a30a52fd3fa878
EngrOdus <henry.odumamwen@outlook.com>
2025-10-08 18:12:37 +0100

    Developed and completed the home page files.

--- week06/scripts/unicon-alu-co.js
+++ week06/scripts/unicon-alu-co.js
@@ -1,18 +1,18 @@
 
-const homePage = document.querySelector(".home-page");
+const homePage = document.querySelector(".home-button");
 
 const roofs = [
     {
         roofName: "Black Metcopo aluminum roofing sheets",
         location: "Edo State, Nigeria",
-        dateDone: "2021, Nov, 10",
+        dateDone: "Nov-10-2021",
         guage: 0.55,
         imageUrl: "images/to-be-used.webp"
     },
     {
         roofName: "Blue longspan aluminum roofing sheet",
         location: "Lagos, Nigeria",
-        dateDone: "2009, Feb, 19",
+        dateDone: "Feb-19-2009",
         guage: 0.45,
         imageUrl: "images/bottom-front-view.webp"
     }
@@ -32,10 +32,12 @@ function createRoofBox(roof) {
     
         image.setAttribute("src", rf.imageUrl); //Set the src attributes for img elements.
         image.setAttribute("alt", rf.roofName); //Set the alt attributes using javascript.
+        image.setAttribute("class", `${rf.dateDone}img`); //Set class attribute for images using their date + img.
         image.setAttribute("loading", "lazy"); //Set the image loading attributes to lazy.
-        leg.innerHTML = `<strong>📛Name:</strong> ${rf.roofName} &nbsp &nbsp <strong>⚖Thichness:</strong> ${rf.guage}mm &nbsp &nbsp <strong>🗺️Location:</strong> ${rf.location} &nbsp &nbsp <strong> 📅 Date:</strong> ${rf.dateDone}`; //Created innerHTML value using javascript.
+        leg.setAttribute("class", `${rf.dateDone}`); //Set class attribute for legend element using corresponding image date.
+        leg.innerHTML = `<strong>📛Name:</strong> ${rf.roofName} &nbsp &nbsp <strong>⚖Thichness:</strong> ${rf.guage}mm &nbsp <strong>🗺️Location:</strong> ${rf.location} &nbsp &nbsp <strong> 📅 Date:</strong> ${rf.dateDone}`; //Created innerHTML value using javascript.
 
-        xtion.appendChild(image); //Appended the img elements and their content to the section elements.
+        leg.appendChild(image); //Appended the img elements and their content to the section elements.
         xtion.appendChild(leg); //Appended the legend elements and their content to the section elements.
 
         document.querySelector(".main").appendChild(xtion); //Append the section element the the main element.
