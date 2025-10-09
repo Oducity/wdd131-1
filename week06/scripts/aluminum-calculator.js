73f846e627928aac56af624187ddd0d7c00c609e
EngrOdus <henry.odumamwen@outlook.com>
2025-10-09 22:21:29 +0100

    Target all calculator inputs and store them in corresponding variables.

---
+++ week06/scripts/aluminum-calculator.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+/*function calculateRoof() {
+    
+}*/
+
+//Get the roofing sheets input
+const roofInput = document.querySelector("#roof-sheets");
+const roofList = roofInput.value;
+
+//Get the cladding sheets input
+const claddingInput = document.querySelector("#cladding-sheets");
+const claddingList = claddingInput.value;
+
+//Get the ceiling sheets input
+const ceilingInput = document.querySelector("#ceiling-sheets");
+const ceilingList = ceilingInput.value;
+
+//Get the ridge input
+const ridgeLInput = document.querySelector("#ridges");
+const ridgeLength = ridgeInput.value;
+const ridgeWInput = document.querySelector("#ridge-width");
+const ridgeWidth = ridgeWInput.value;
+
+//Get the top-trimmers input
+const topTrimmerInput = document.querySelector("#top-trimmer-length");
+const topTrimmerLength = topTrimmerInput.value;
+const topTrimmerwidthInput = document.querySelector("#top-trimmers-width");
+const topTrimmerWidth = topTrimmerwidthInput.value;
+
+//Get the Badge-board input
+const bBLengthInput = document.querySelector("#badge-board-length");
+const bBlength = bBLengthInput.value;
+const bBWidthInput = document.querySelector("#badge-board-width");
+const bBWidth = bBWidthInput.value;
+
+//Get the gutter trimmers input
+const gTrimmersLength = document.querySelector("#gutter-trimmers");
+const gutterTrimmerLength = gTrimmersLength.value;
+const gTrimmerWidth = document.querySelector("#gutter-trimmers-width");
+const gutterTrimmerWidth = gTrimmerWidth.value;
+
+//Get the bottom-trimmers input
+const bottomTrimmersL = document.querySelector("#bottom-trimmers");
+const bottomTL = bottomTrimmersL.value;
+const bottomTrimmerW = document.querySelector("#bottom-trimmers-width");
+const bottomTW = bottomTrimmerW.value;
+
+//Get the wall-trimmers input
+const wallTLength = document.querySelector("#wall-trimmers");
+const wTLength = wallTLength.value;
+const wallTWidth = document.querySelector("#wall-trimmers-width");
+const wTWidth = wallTWidth.value;
+
+//Get the gutter input
+const gutterLInput = document.querySelector("#gutters");
+const gutterLength = gutterLInput.value;
+const gutterWInput = document.querySelector("#gutter-width");
+const gutterWidth = gutterWInput.value;
+
