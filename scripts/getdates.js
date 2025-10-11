446caef5b980f644fbbbdd401294f28b94c3b8a7
EngrOdus <henry.odumamwen@outlook.com>
2025-10-11 23:51:13 +0100

    Added padding to footer.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,6 +3,6 @@ let currentYear = date.getFullYear();
 let text = currentYear.toString();
 document.getElementById("currentyear").textContent = text;
 
-let lastModify = date(document.lastModified);
+let lastModify = date.document.lastModified;
 let lastModifyString = "Last Updated:" + " " + lastModify.toString();
 document.getElementById("lastModified").textContent = lastModifyString;
