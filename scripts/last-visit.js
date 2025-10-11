8a67c011e840237bd6533c9918e3d33f7ff2f31d
EngrOdus <henry.odumamwen@outlook.com>
2025-10-11 13:50:15 +0100

    Linked last-visit.js to unicon html files.

--- scripts/last-visit.js
+++ scripts/last-visit.js
@@ -3,9 +3,9 @@ const visitBox = document.getElementById("visit-times");
 let numOfVisit = Number(window.localStorage.getItem("numOfVisit-Is")) || 0;
 
 if (numOfVisit !== 0) {
-    visitBox.textContent = numOfVisit;
+    visitBox.innerText = numOfVisit;
 } else {
-    visitBox.innerText = "Welcome. Enjoy your first visit.";
+    visitBox.innerText = "Welcome, Enjoy your first visit.";
 };
 
 numOfVisit++;
