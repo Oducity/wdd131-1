4f67f82ba1c1f30a294ceb7b6f5fc0e4f4165682
EngrOdus <henry.odumamwen@outlook.com>
2025-10-11 22:48:34 +0100

    modified getdate.js file.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,8 @@
 const date = new Date();
-let currentYear = new Date.getFullYear();
+let currentYear = date.getFullYear();
 let text = currentYear.toString();
-document.getElementById("currentyear").innerHTML = text;
+document.getElementById("currentyear").textContent = text;
 
 let lastModify = date(document.lastModified);
 let lastModifyString = "Last Updated:" + " " + lastModify.toString();
-document.getElementById("lastModified").innerHTML = lastModifyString;
+document.getElementById("lastModified").textContent = lastModifyString;
