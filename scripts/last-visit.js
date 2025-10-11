f03c1a765242a2dff3405734b527d9c2b322b6db
EngrOdus <henry.odumamwen@outlook.com>
2025-10-11 13:43:12 +0100

    Developed last-visit.js file.

---
+++ scripts/last-visit.js
@@ -0,0 +1,13 @@
+const visitBox = document.getElementById("visit-times");
+
+let numOfVisit = Number(window.localStorage.getItem("numOfVisit-Is")) || 0;
+
+if (numOfVisit !== 0) {
+    visitBox.textContent = numOfVisit;
+} else {
+    visitBox.innerText = "Welcome. Enjoy your first visit.";
+};
+
+numOfVisit++;
+
+window.localStorage.setItem("numberOfVisit-Is", numOfVisit);
