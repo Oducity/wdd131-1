0798675cdcce205015f591b4e5e73f6dd063a12e
EngrOdus <henry.odumamwen@outlook.com>
2025-10-11 22:24:07 +0100

    Addaed code for date computation.

--- scripts/unicon-alu-co.js
+++ scripts/unicon-alu-co.js
@@ -1,5 +1,12 @@
 
+const date = new Date();
+let currentYear = new Date.getFullYear();
+let text = currentYear.toString();
+document.getElementById("currentyear").innerHTML = text;
 
+let lastModify = date(document.lastModified);
+let lastModifyString = "Last Updated:" + " " + lastModify.toString();
+document.getElementById("lastModified").innerHTML = lastModifyString;
 
 const roofs = [
     {
